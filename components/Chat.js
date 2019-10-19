02142f605447cdece2c4e599b9b68b0335d25652
Günther von Häfen <guenther.vonhaefen@gmx.de>
2019-10-19 19:54:21 +0200

    5.5: added offline functionality with asyncStorage and NetInfo.

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,7 @@
 //import react component
 import React, { Component } from 'react';
 //import relevant components from react native
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, AsyncStorage, NetInfo } from 'react-native';
 // import gifted chat
 import { GiftedChat } from 'react-native-gifted-chat';
 //import keyboardspacer
@@ -32,22 +32,68 @@ export default class Chat extends Component {
 
     this.state = {
       messages: [],
-      uid: 0
+      uid: 0,
+      isConnected: false
     };
   }
 
-  componentDidMount() {
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        firebase.auth().signInAnonymously();
-      }
-
+  // get messages from asyncStorage
+  async getMessages() {
+    let messages = '';
+    try {
+      messages = await AsyncStorage.getItem('messages') || [];
       this.setState({
-        uid: user.uid,
-        messages: []
+        messages: JSON.parse(messages)
       });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-      this.unsubscribe = this.referenceChatMessages.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
+  // save messages in asyncStorage
+  async saveMessages() {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // delete messages from asyncStorage
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem('messages');
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  componentDidMount() {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      if (isConnected) {
+        this.setState({
+          isConnected: false,
+        });
+
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+          if (!user) {
+            firebase.auth().signInAnonymously();
+          }
+
+          this.setState({
+            uid: user.uid,
+            messages: []
+          });
+
+          this.unsubscribe = this.referenceChatMessages.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
+        });
+      } else {
+        this.setState({
+          isConnected: false,
+        });
+
+        this.getMessages();
+      }
     });
   }
 
@@ -96,8 +142,21 @@ export default class Chat extends Component {
       messages: GiftedChat.append(previousState.messages, messages),
     }), () => {
       this.addMessage();
+      this.saveMessages();
     });
-  }
+  };
+
+  // hide inputbar when offline
+  renderInputToolbar(props) {
+    if (this.state.isConnected == false) {
+    } else {
+      return(
+        <InputToolbar
+        {...props}
+        />
+      );
+    }
+  };
 
   //render components
   render() {
